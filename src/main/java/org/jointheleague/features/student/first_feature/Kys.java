ba30a5ae4b1bfbdac6f6b95f01249c8ee4449513
Evan Keri <Evan Keri@DESKTOP-2O56U0E.sdcs.local>
2025-02-07 13:37:36 -0800

    added exit video on kys

--- src/main/java/org/jointheleague/features/student/first_feature/Kys.java
+++ src/main/java/org/jointheleague/features/student/first_feature/Kys.java
@@ -1,13 +1,19 @@
 package org.jointheleague.features.student.first_feature;
 
+import java.io.File;
+import java.util.Random;
+
 import org.jointheleague.api_wrapper.ReceivedMessage;
 import org.jointheleague.features.abstract_classes.Feature;
 import org.jointheleague.features.help_embed.plain_old_java_objects.help_embed.HelpEmbed;
 
+import net.dv8tion.jda.api.utils.FileUpload;
+
 public class Kys extends Feature {
 
     public final String COMMAND = "onion kys";
-
+    File kysVid = new File("src/main/java/org/jointheleague/onionkys.mp4");
+    FileUpload fu = FileUpload.fromData(kysVid);
     public Kys(String channelName) {
         super(channelName);
 
@@ -22,7 +28,9 @@ public class Kys extends Feature {
         if (messageContent.contains("onion")) {
         	if(messageContent.contains("kys")||messageContent.contains("kill yourself")) {
             //respond to message here
-        		//event.getEvent().getChannel().sendFiles(null).submit().join();
+        		if(new Random().nextInt(10)==2) {
+        	event.getEvent().getChannel().sendFiles(fu).submit().join();
+        		}
             event.sendResponse("killing self...");
             System.exit(0);
         	}
